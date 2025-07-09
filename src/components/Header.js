243e07726c6c41e7fdb6152ef95197254f64b897
kisa-fatima <kisafatima171@gmail.com>
2025-07-08 23:56:14 -0700

    header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,16 +6,18 @@ import '../styles/Global.css';
 function Header() {
   return (
     <header className="header">
-      <div className="header__logo">SHOP.CO</div>
-      <nav className="header__nav">
-        <Link to="/" className="header__link">Home</Link>
-        <Link to="/women" className="header__link">Women</Link>
-        <Link to="/men" className="header__link">Men</Link>
-        <Link to="/kids" className="header__link">Kids</Link>
-      </nav>
-      <div className="header__search-container">
-        <span className="header__search-icon"><FaSearch /></span>
-        <input className="header__search" type="text" placeholder="Search for products..." />
+      <div className="header__center">
+        <div className="header__logo">SHOP.CO</div>
+        <nav className="header__nav">
+          <Link to="/" className="header__link">Home</Link>
+          <Link to="/women" className="header__link">Women</Link>
+          <Link to="/men" className="header__link">Men</Link>
+          <Link to="/kids" className="header__link">Kids</Link>
+        </nav>
+        <div className="header__search-container">
+          <span className="header__search-icon"><FaSearch /></span>
+          <input className="header__search" type="text" placeholder="Search for products..." />
+        </div>
       </div>
       <div className="header__icons">
         <span className="header__icon" title="Cart"><FaShoppingCart /></span>
