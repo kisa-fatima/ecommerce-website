33e414df0f6d8b850d576014c22fed301d6b4e5a
fatima07052024 <fatimaa.h1020@gmail.com>
2025-07-10 00:48:19 -0700

    footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import HomeHero from '../components/HomeHero';
+import '../styles/Home.css';
 
 const Home = () => {
   return (
-    <>
+    <div className="home-content-with-gap">
       <HomeHero />
-    </>
+    </div>
   );
 }
 
