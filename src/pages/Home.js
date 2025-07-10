5bf1d052c0837f228fb06cf8ff6064bc5aaf27f9
Zainab Hassan <z.hhassan567@gmail.com>
2025-07-10 02:20:32 -0700

    Update UI components and styles: testimonials slider, mobile responsiveness, and homepage sections

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import HomeHero from '../components/HomeHero';
 import BrandLogos from '../components/BrandLogos';
+import NewArrivals from '../components/NewArrivals';
+import TopSelling from '../components/TopSelling';
+import BrowseByStyle from '../components/BrowseByStyle';
+import HappyCustomers from '../components/HappyCustomers';
 
 const Home = () => {
   return (
     <div>
       <HomeHero />
       <BrandLogos />
+      <NewArrivals />
+      <TopSelling />
+      <BrowseByStyle />
+      <HappyCustomers />
     </div>
   );
 }
