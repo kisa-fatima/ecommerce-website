20e04edd176fae5dc9bf53efbf19a32f53fce052
kisa-fatima <kisafatima171@gmail.com>
2025-07-10 02:42:07 -0700

    women banner done

--- src/pages/Women.js
+++ src/pages/Women.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import PageBanner from '../components/PageBanner';
+import womenImg from '../assets/images/women.jpg';
 
 const Women = () => {
   return (
-    <div>Women</div>
-  )
+    <div>
+      <PageBanner
+        image={womenImg}
+        title="Women"
+        description="Discover our curated collection of women's fashion designed to empower every style. From breezy summer dresses to bold leather jackets, explore outfits made for work, weekends, and everything in between. Whether you're dressing up or keeping it casual, find the perfect blend of comfort, confidence, and charm."
+      />
+    </div>
+  );
 }
 
-export default Women
+export default Women;
