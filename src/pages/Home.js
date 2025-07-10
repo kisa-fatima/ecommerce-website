790637573370b3077f4a3b495a1c701eac237abb
kisa-fatima <kisafatima171@gmail.com>
2025-07-10 01:21:33 -0700

    gutter

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import HomeHero from '../components/HomeHero';
-<<<<<<< HEAD
 import '../styles/Home.css';
+import BrandLogos from '../components/BrandLogos';
 
 const Home = () => {
   return (
     <div className="home-content-with-gap">
       <HomeHero />
-=======
-import BrandLogos from '../components/BrandLogos';
-
-const Home = () => {
-  return (
-    <div>
-      <HomeHero />
       <BrandLogos />
->>>>>>> 123f2211e41b162590c8115ee1c41b65c1524088
     </div>
   );
 }
