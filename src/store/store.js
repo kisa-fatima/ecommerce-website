069b9e9d5dc00f1ea82029669b591b7a1246f66b
kisa-fatima <kisafatima171@gmail.com>
2025-07-18 04:53:44 -0700

    refesher

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,34 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import authReducer from './authSlice';
 import cartReducer from './cartSlice';
 import userReducer from './userSlice';
 import authRootSaga from './authSaga';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const sagaMiddleware = createSagaMiddleware();
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth', 'cart', 'user'],
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  cart: cartReducer,
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    cart: cartReducer,
-    user: userReducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ thunk: false, serializableCheck: false }).concat(sagaMiddleware),
 });
 
 sagaMiddleware.run(authRootSaga);
 
+export const persistor = persistStore(store);
 export default store; 
